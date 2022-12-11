b29a1c95d4b1999985e89646f455202df9b071ab
LucasDCoelho <94465173+LucasDCoelho@users.noreply.github.com>
2022-12-10 21:29:05 -0300

    update: add scripts and finished navbar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,29 @@
 function toggle(){
+    let nav = document.getElementById('nav');
     let ul = document.getElementById('nav-menu');
     let btn_toggle = document.getElementById('toggle');
 
     if(ul.classList.contains('active')){
+        nav.classList.remove('active');
         ul.classList.remove('active');
-        btn_toggle.innerText = 'close';
-    } else{
-        ul.classList.add('active');
         btn_toggle.innerText = 'menu';
+    } else{
+        nav.classList.add('active');
+        ul.classList.add('active');
+        btn_toggle.innerText = 'close';
     }
 }
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
